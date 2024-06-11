69150ec146180e8e2724673f84037bf640b5f0a9
DanielaSMR <10828775@ieselgrao.org>
2024-06-11 19:19:58 +0200

    g

--- src/Lista.java
+++ src/Lista.java
@@ -1,28 +1,28 @@
 public class Lista{
 // Eficiencia, inteligibilidad y orden del código.
     // Orientación  a objetos: 
-        // --herencia y herencia múltiple
-        // --encapsulación
-        // sobrecarga
-        // polimorfismo
+        //-- herencia y herencia múltiple
+        // --encapsulación(int no --> integer)
+        // --sobrecarga
+        // --polimorfismo
         // --visibilidad
         // --constructores
-        // ~Abstractos y finales
+        // --Abstractos y finales
         // ~Estático y dinámico...
-        // Comparar objetos (implements Comparable)
-// Depuración
+        // --Comparar objetos (implements Comparable)
+//-- Depuración
 // --Cast o conversión de tipos
-// --Estructuras de control
+// --Estructuras de control(for/while)
 // --Arrays y colecciones
-// --lgorítmica
+// ~Algorítmica
 // Excepciones
-    // Creación
-    // Manejo
+    // --Creación
+    // --Manejo
     // propagación
     // captura
 // Persistencia de datos
-    // --Ficheros binarios
+    // Ficheros binarios
     // Ficheros de texto
-    // --Serialización
-    // --Acceso modificación y consulta a BD relacionales. Transacciones
+    // Serialización
+    // Acceso modificación y consulta a BD relacionales. Transacciones
 }
